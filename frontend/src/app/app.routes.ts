b44a83cd57be737266ab4cc312e767da303aab71
aminnak <aminamirikeivani@gmail.com>
2025-02-20 15:20:34 +0330

    feat: Add 404 route to the front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./pages/home-component/home.component";
 import { ServicesComponent } from './pages/service-component/services.component';
 import { SignInComponent } from './pages/sign-in-component/sign-in.component';
-
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
     {path : '' , redirectTo : '/home' , pathMatch : 'full'},
     {path : 'home' , component : HomeComponent},
     {path : 'services' , component : ServicesComponent},
     {path : 'sign-in' , component : SignInComponent},
+    {path : 'not-found' , component : NotfoundComponent},
+    {path : '**' , redirectTo : '/not-found' ,pathMatch : 'full'},
 ];
